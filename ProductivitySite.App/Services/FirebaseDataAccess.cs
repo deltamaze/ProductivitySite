f81b014284673e8fefd69f2ef82229bd0a7e2b8a
deltamaze <wcpooley@gmail.com>
2019-02-07 19:53:00 -0600

    adding firestore

--- ProductivitySite.App/Services/FirebaseDataAccess.cs
+++ ProductivitySite.App/Services/FirebaseDataAccess.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Google;
 
 namespace ProductivitySite.App.Services
 {
     public class FirebaseDataAccess : IDataAccess 
     {
+
     }
 }
--- ProductivitySite.App/Services/IDataAccess.cs
+++ ProductivitySite.App/Services/IDataAccess.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Google.Apis.Firestore;
-
 namespace ProductivitySite.App.Services
 {
     public interface IDataAccess
