156e1fe1fa4d0d0e69dbdc41b2a72e62df4ac2b2
deltamaze <wcpooley@gmail.com>
2019-03-06 23:28:12 -0600

    updated firestore reference

--- ProductivitySite.App/Services/FirebaseDataAccess.cs
+++ ProductivitySite.App/Services/FirebaseDataAccess.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Google;
+using Google.Cloud.Firestore;
 
 namespace ProductivitySite.App.Services
 {
     public class FirebaseDataAccess : IDataAccess 
     {
+        [FirestoreData]
+        public class Employee
+        {
+            public string EmployeeId { get; set; }
+            public DateTime date { get; set; }
+            [FirestoreProperty]
+            public string EmployeeName { get; set; }
+            [FirestoreProperty]
+            public string CityName { get; set; }
+            [FirestoreProperty]
+            public string Designation { get; set; }
+            [FirestoreProperty]
+            public string Gender { get; set; }
+        }
 
     }
 }
