9121a295406155c552aff658f9be2e8de6973bd4
deltamaze <wcpooley@gmail.com>
2019-10-06 20:09:35 -0500

    merge issue

---
+++ blazor/productivityPlanner/Services/FirebaseService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace productivityPlanner.Services
+{
+    public class FirebaseService
+    {
+        public string firebaseConfig { get; set; }
+
+        private string @"https://identitytoolkit.googleapis.com/";
+
+    }
+}
