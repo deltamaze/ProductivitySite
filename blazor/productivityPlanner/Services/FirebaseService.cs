ba432050e036ae31bd932c151fb6efd3fba7563c
deltamaze <wcpooley@gmail.com>
2019-10-06 23:53:59 -0500

    config setup

--- blazor/productivityPlanner/Services/FirebaseService.cs
+++ blazor/productivityPlanner/Services/FirebaseService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net.Http;
+using System.Configuration;
 
 namespace productivityPlanner.Services
 {
@@ -10,8 +11,8 @@ namespace productivityPlanner.Services
     {
         public string firebaseConfig { get; set; }
 
-        private const string BaseUrl = @"https://identitytoolkit.googleapis.com/";
-
+        private const string BaseUrl = @"https://identitytoolkit.googleapis.com";
+        private const string ApiKey = ConfigurationManager
         static async Task SignUp()
         {
             using (var client = new HttpClient())
@@ -19,7 +20,9 @@ namespace productivityPlanner.Services
                 client.BaseAddress = new Uri(BaseUrl);
                 var content = new FormUrlEncodedContent(new[]
                 {
-                new KeyValuePair<string, string>("", "login")
+                new KeyValuePair<string, string>("email", "test@gmail.com"),
+                new KeyValuePair<string, string>("password", "test@gmail.com"),
+                new KeyValuePair<string, string>("returnSecureToken", "test@gmail.com")
             });
                 var result = await client.PostAsync("/v1/accounts:signUp?key=[API_KEY]", content);
                 string resultContent = await result.Content.ReadAsStringAsync();
