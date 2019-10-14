0d339ac21f878320359e82dec6c4799190ed2282
deltamaze <wcpooley@gmail.com>
2019-10-14 00:12:32 -0500

    working on changing target date

--- react/src/pages/planner.js
+++ react/src/pages/planner.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setDate } from '../services/targetDate/action';
+import { getFormattedDate } from '../utilities/dateFormatter';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class PlannerPage extends React.Component {
@@ -10,17 +12,29 @@ class PlannerPage extends React.Component {
     // this.props.fetchAuth();
   }
 
+  incrementDate(direction, date) {
+    const day = direction * (1000 * 60 * 60 * 24);
+    const msDate = parseInt(date, 10);
+    const parsedDate = new Date((msDate - day));
+    this.props.setDate(parsedDate);
+  }
+
   render() {
     return (
-      <div>
-        PlannerPage
-      </div>
+      <>
+        <ul className="list-group list-group-horizontal">
+          <li className="list-group-item">&lt;&lt;</li>
+          <li className="list-group-item">{getFormattedDate(this.props.targetDate.date)}</li>
+          <li className="list-group-item"><button onClick={() => this.incrementDate(1, this.props.targetDate.date)}>&gt;&gt;</button></li>
+        </ul>
+      </>
     );
   }
 }
 
 export default connect(
-  state => ({ auth: state.auth }),
+  (state) => ({ auth: state.auth, targetDate: state.targetDate }),
   ({
+    setDate
   })
 )(PlannerPage);
