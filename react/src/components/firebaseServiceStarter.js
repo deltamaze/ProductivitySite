be7f2f67e97e46f11d4398bf972c439f6e366b00
deltamaze <wcpooley@gmail.com>
2019-10-15 07:51:35 -0500

    started implementing getmonthyear function

--- react/src/components/firebaseServiceStarter.js
+++ react/src/components/firebaseServiceStarter.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchAuth } from '../services/auth/action';
 import { fetchCalendar } from '../services/calendar/action';
+import { getMonthYear } from '../utilities/dateFormatter';
 
 class FirebaseServiceStarter extends React.Component {
   componentDidMount() {
     this.props.fetchAuth();
     this.props.fetchCalendar(this.props.date);
-    console.log('test that this does not get called after date changes');
   }
 
   componentDidUpdate(prevProps) {
     // Typical usage (don't forget to compare props):
-    if (this.props.targetDate !== prevProps.targetDate.date) {
-      console.log(`prevVal=${prevProps.date}`);
-      console.log(`curVal=${this.props.date}`)
-      this.fetchData(this.props.userID);
+    console.log(getMonthYear(this.props.targetDate));
+    console.log(getMonthYear(prevProps.targetDate.date));
+    if (getMonthYear(this.props.targetDate) !== getMonthYear(prevProps.targetDate.date)) {
+      console.log('month change detected');
     }
   }
 
