caeae0eee008d665573da3e4b1fbc5140c3b7e92
deltamaze <wcpooley@gmail.com>
2019-10-23 22:45:31 -0500

    button to add item

--- react/src/components/browseItems.js
+++ react/src/components/browseItems.js
@@ -2,13 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class BrowseItems extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showTextBox: false
+    };
+  }
+
   componentDidMount() {
   }
 
 
   render() {
     return (
-      <>BrowseItems, {this.props.itemType}
+      <>
+        <div>
+          <button className="btn btn-sm btn-success " onClick={() => this.setState({ showTextBox: true })}>
+            Add New {this.props.itemType.charAt(0).toUpperCase() + this.props.itemType.slice(1)}
+          </button>
+          <div>
+            {this.state.showTextBox // condition
+              // eval True
+              ? <div>Hello world</div>
+              // eval False
+              : null}
+          </div>
+        </div>
+        <br />
+        BrowseItems, {this.props.itemType}
         Files:
         {
           this.props.itemIndex.items
--- react/src/pages/browseNotes.js
+++ react/src/pages/browseNotes.js
@@ -2,13 +2,6 @@ import React from 'react';
 import BrowseItems from '../components/browseItems';
 
 class BrowseNotesPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showTextBox: false
-    };
-  }
-
   componentDidMount() {
   }
 
