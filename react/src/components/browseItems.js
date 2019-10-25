bf8344553f6f9c345dc3522b39fe80edbafe2c82
deltamaze <wcpooley@gmail.com>
2019-10-24 23:56:06 -0500

    make add button work differently

--- react/src/components/browseItems.js
+++ react/src/components/browseItems.js
@@ -2,31 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class BrowseItems extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showTextBox: false
-    };
-  }
-
   componentDidMount() {
   }
 
-
+  // TODO make onClick for Add New Item create the item, auto generate a Title, and navigate user to that item page
   render() {
     return (
       <>
         <div>
-          <button className="btn btn-sm btn-success " onClick={() => this.setState({ showTextBox: true })}>
+          <button className="btn btn-sm btn-success " onClick={() => console.log('test')}>
             Add New {this.props.itemType.charAt(0).toUpperCase() + this.props.itemType.slice(1)}
           </button>
-          <div>
-            {this.state.showTextBox // condition
-              // eval True
-              ? <div>Hello world</div>
-              // eval False
-              : null}
-          </div>
+
         </div>
         <br />
         BrowseItems, {this.props.itemType}
