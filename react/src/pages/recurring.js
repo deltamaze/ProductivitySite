a3beba2f0df133dcfb654b7a85f74ab5a7abe369
deltamaze <wcpooley@gmail.com>
2019-12-11 23:37:35 -0600

    workin on recursvie page

--- react/src/pages/recurring.js
+++ react/src/pages/recurring.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
 
 
 class RecurringPage extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      eventModalShow: false,
+      targetRecurEventId: '',
+      targetRecurEventTitle: ''
+    };
+  }
   componentDidMount() {
     // this.props.fetchAuth();
+  }
+
+  showEventModal(eventId, itemTitle) {
+    this.setState({
+      modalShow: true,
+      targetItemId: itemId,
+      targetItemTitle: itemTitle
+    });
+  }
+  showEventModal(itemId, itemTitle) {
+    this.setState({
+      modalShow: true,
+      targetItemId: itemId,
+      targetItemTitle: itemTitle
+    });
   }
 
   render() {
     return (
       <div>
         RecurringPage
+        <Button
+          className=""
+          variant="outline-danger"
+          size="sm"
+          onClick={() => this.showConfirmDeleteModal(value.id, value.data().itemTitle)}
+        >
+                      New Recursive Event
+        </Button>
       </div>
     );
   }
