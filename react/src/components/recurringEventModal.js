fdd0fd119f9b0b97a71a5c181277348ae96e2bd1
deltamaze <wcpooley@gmail.com>
2020-01-03 16:43:39 -0600

    remove hasloaded requirement from recur modal

--- react/src/components/recurringEventModal.js
+++ react/src/components/recurringEventModal.js
@@ -5,36 +5,7 @@ import { Modal, Button } from 'react-bootstrap';
 // import { getMonthName } from '../utilities/dateHelper';
 
 class RecurringEventModal extends React.Component {
-
-  //on change events for each field to update store
-  
-  componentDidUpdate() {
-    // if eventid zero, reset state to default
-    if(this.props.eventId === '0')
-    {
-      this.setState({
-        targetRecurEventId: eventId,
-        targetRecurEventTitle: eventTitle,
-        eventModalShow: true,
-        eventDeleteModalShow: false,
-      });
-
-    }
-    // else pull event data from event store
-    console.log('test');
-    console.log(this.props.eventId);
-  }
-
-  setLocalState() {
-    // this.props.fetchAuth();
-    console.log(this.props.eventId);
-    if (this.props.eventId === '0') {
-      // use default values, and set hasLoaded
-      this.setState({
-        hasLoaded: true // handle input and update textbox
-      });
-    }
-  }
+  // on change events for each field to update store
 
   render() {
     if (this.props.eventId !== '0') {
@@ -55,21 +26,16 @@ class RecurringEventModal extends React.Component {
         </Modal.Header>
         <Modal.Body>
           <>
-            {(!this.state.hasLoaded) ? (
-              <div>Loading...</div>
-            ) : (
-              <p>
-                  Are you sure you would like to delete: <strong>{this.props.itemTitle}</strong>
-                <br /><br /><br />
-                <Button variant="danger" size="lg" block onClic k={this.onHideWithDelete}>
-                    Yes, Delete
-                </Button>
-                <Button variant="primary" size="lg" block onClick={this.onHide}>
-                    No, Cancel
-                </Button>
-              </p>
-
-            )}
+            <p>
+              Are you sure you would like to delete: <strong>{this.props.itemTitle}</strong>
+              <br /><br /><br />
+              <Button variant="danger" size="lg" block onClic k={this.onHideWithDelete}>
+                Yes, Delete
+              </Button>
+              <Button variant="primary" size="lg" block onClick={this.onHide}>
+                No, Cancel
+              </Button>
+            </p>
           </>
         </Modal.Body>
       </Modal>
