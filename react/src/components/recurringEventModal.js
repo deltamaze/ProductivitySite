9ffeaa1081b3fdbd8bcdd1c9d05172fbfb32d195
deltamaze <wcpooley@gmail.com>
2020-01-15 12:57:24 -0600

    added keys to remove browser errors

--- react/src/components/recurringEventModal.js
+++ react/src/components/recurringEventModal.js
@@ -7,24 +7,25 @@ import { getRepeatOptions, getDays, getMonths } from '../utilities/enums';
 class RecurringEventModal extends React.Component {
   constructor(props) {
     super(props);
-    this.repeatOptions = getRepeatOptions().map((item) => <option value={item}>{item}</option>);
+    this.repeatOptions = getRepeatOptions()
+      .map((item) => <option key={item} value={item}>{item}</option>);
     this.daysOfWeekCheckBox = getDays().map((day) => (
-      <div>
-        <input id={`dayCheckBox${day}`} type="checkbox" />
+      <div key={`dayCheckBox${day}`}>
+        <input onChange={this.handleChange} id={`dayCheckBox${day}`} type="checkbox" />
         <label htmlFor={`dayCheckBox${day}`}>{day}</label>
 
       </div>
     ));
-    this.dayOfWeekCheckBoxGroup = <div>Choose Day of Weeks {this.daysOfWeekCheckBox}</div>;
+    this.dayOfWeekCheckBoxGroup = <div key="weeks">Choose Day of Weeks {this.daysOfWeekCheckBox}</div>;
 
     this.monthCheckBox = getMonths().map((month) => (
-      <div>
-        <input id={`monthCheckBox${month}`} type="checkbox" />
+      <div key={`monthCheckBox${month}`}>
+        <input onChange={this.handleChange} id={`monthCheckBox${month}`} type="checkbox" />
         <label htmlFor={`monthCheckBox${month}`}>{month}</label>
 
       </div>
     ));
-    this.monthCheckBoxGroup = <div>Choose Month {this.monthCheckBox}</div>;
+    this.monthCheckBoxGroup = <div key="months">Choose Month {this.monthCheckBox}</div>;
 
     this.state = {
       allDayFlag: true
@@ -33,6 +34,9 @@ class RecurringEventModal extends React.Component {
   // on change events for each field to update store
 
   handleChange(event) {
+    console.log(event);
+    // review https://www.pluralsight.com/guides/handling-multiple-inputs-with-single-onchange-handler-react
+    
     // determine what is changing
     // event title
     // description
@@ -41,16 +45,6 @@ class RecurringEventModal extends React.Component {
     // weekPartSelection
     // endDate
     // monthPartSelection
-    this.setState({
-      mainTextArea: event.target.value // handle input and update textbox
-    });
-    const day = getDayNumber(this.props.selectedDate.date);
-    const newMonth = generateMonthPayload(day,
-      this.props.element,
-      event.target.value,
-      this.props.month.monthData);
-    // package us a new month object to post back to firebase
-    this.setMonthWithDebouce(newMonth, this.props.month.monthRef, this.props.auth.uid);
   }
 
   render() {
@@ -69,84 +63,86 @@ class RecurringEventModal extends React.Component {
         </Modal.Header>
         <Modal.Body>
           <>
-            <p>
-              {/* Event Title */}
+            {/* Event Title */}
+            <input
+              className="mainTexBoxInput"
+              type="text"
+              placeholder="Event Title"
+              value={this.props.recurringEvents.title}
+              onChange={this.handleChange}
+            />
+            {/* Description */}
+            <textarea
+              id="Description"
+              className="modalTextArea"
+              placeholder="Description"
+              value={this.props.recurringEvents.description}
+              onChange={this.handleChange}
+            />
+            {/* Start Date Time */}
+              StartTime
+            <div>
               <input
-                className="mainTexBoxInput"
-                type="text"
-                placeholder="Event Title"
-                value={this.props.recurringEvents.title}
-                onChange={this.handleTitleChange}
-              />
-              {/* Description */}
-              <textarea
-                className="modalTextArea"
-                placeholder="Description"
-                value={this.props.recurringEvents.description}
+                type="date"
+                value="2018-07-22"
                 onChange={this.handleChange}
               />
-              {/* Start Date Time */}
-              StartTime
-              <div>
+              <input
+                type="time"
+                value="13:30"
+                onChange={this.handleChange}
+              />
+              <label htmlFor="allDay">
                 <input
-                  type="date"
-                  value="2018-07-22"
+                  type="checkbox"
+                  id="allDay"
+                  name="allDay"
+                  checked
+                  onChange={this.handleChange}
                 />
-                <input
-                  type="time"
-                  value="13:30"
-                />
-                <label htmlFor="allDay">
-                  <input
-                    type="checkbox"
-                    id="allDay"
-                    name="allDay"
-                    checked
-                  />
                   All Day Event
-                </label>
-              </div>
-              {/* Repeat Every [Frequency Nuber][Frequency Type(Day,Week,Month,Year)] */}
-              <div>
+              </label>
+            </div>
+            {/* Repeat Every [Frequency Nuber][Frequency Type(Day,Week,Month,Year)] */}
+            <div>
                 Repeat Every
-                <select id="myList">
-                  {this.repeatOptions}
-                </select>
-              </div>
-              {/* WeekPart Selection (Weekly)(Sun,Mon,Tues,etc..) */}
-              {
-                this.props.recurringEvents.frequencyType === 'Weekly' ? this.dayOfWeekCheckBoxGroup : null
-              }
-              {/* Month Part Selection (Jan/Feb/March) */}
-              {
-                this.props.recurringEvents.frequencyType === 'Monthly' ? this.monthCheckBoxGroup : null
-              }
-              {/* MonthType Selection (Monthly on Day (day number of startdate) / Monthly
+              <select onChange={this.handleChange} id="myList">
+                {this.repeatOptions}
+              </select>
+            </div>
+            {/* WeekPart Selection (Weekly)(Sun,Mon,Tues,etc..) */}
+            {
+              this.props.recurringEvents.frequencyType === 'Weekly' ? this.dayOfWeekCheckBoxGroup : null
+            }
+            {/* Month Part Selection (Jan/Feb/March) */}
+            {
+              this.props.recurringEvents.frequencyType === 'Monthly' ? this.monthCheckBoxGroup : null
+            }
+            {/* MonthType Selection (Monthly on Day (day number of startdate) / Monthly
                on ([1st/2nd] datepart) on month) */}
-              {
-                this.props.recurringEvents.frequencyType === 'Monthly'
-                  ? (
-                    <div>
+            {
+              this.props.recurringEvents.frequencyType === 'Monthly'
+                ? (
+                  <div>
                       Repeat on Day Number or Nth Day of Week?
-                      <select id="myList">
-                        <option>Day Number</option>
-                        <option>Nth Day of Week</option>
-                      </select>
-                    </div>
-                  ) : null
-              }
+                    <select onChange={this.handleChange} id="myList">
+                      <option>Day Number</option>
+                      <option>Nth Day of Week</option>
+                    </select>
+                  </div>
+                ) : null
+            }
 
-              {/* End Time ? Never RadioButton, or DateTime field.
+            {/* End Time ? Never RadioButton, or DateTime field.
                If Never RadioButton, clear out store.EndDate to undefined, and disable field */}
 
 
-              <Button variant="success" size="lg" block onClick={this.props.onHideWithUpsert}>
+            <Button variant="success" size="lg" block onClick={this.props.onHideWithUpsert}>
                 Save
-              </Button>
-              <Button variant="danger" size="lg" block onClick={this.props.onHide}>
+            </Button>
+            <Button variant="danger" size="lg" block onClick={this.props.onHide}>
                 Cancel
-              </Button>
-            </p>
+            </Button>
           </>
         </Modal.Body>
       </Modal>
